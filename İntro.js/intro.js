4c4a9dae1de793395d13b0159ae31ebff904dee1
AtillaErdogan <mr.erdgn1999@gmail.com>
2022-02-23 21:23:45 +0300

    intro.js

---
+++ İntro.js/intro.js
@@ -0,0 +1,17 @@
+// Toplu Yorum Satırı (CTRL + K + C)
+//FonkSiyon
+function GenerateRandomNumber(Limit = 49){
+    return Number = Math.ceil(Math.random() * Limit)
+     
+}
+//Şart Blokları
+var ifNumber=2;
+if(ifNumber<50)
+{
+console.log("Sayı 50 Değerinden Küçük");
+}
+//For  
+for (let i = 1; i <= 10; i++) {
+    console.log(GenerateRandomNumber())
+    
+}
