2cb8c61d0346013da6e15f8b2f32917498824db9
AtillaErdogan <mr.erdgn1999@gmail.com>
2023-01-12 03:37:31 +0300

    12.01.2023

---
+++ JSDemos/JCStarterKit/Array.js
@@ -0,0 +1,6 @@
+let Krediler = ["Konut Kredisi","Tasıt Kredisi","Bireysel Kredisi"]
+console.log("<ul>");
+for (let i = 0; i < Krediler.length; i++) {
+    console.log("<li>" + Krediler[i] + "</li>");   
+}
+console.log("</ul>");
